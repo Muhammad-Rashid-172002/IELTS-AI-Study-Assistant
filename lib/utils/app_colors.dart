12426aa7667418acf2f74d1396a256f7dc1cd453
M.Rashid <muhammadrashid172002@gmail.com>
2025-11-23 19:19:52 +0500

    added commit

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+// Define your app colors here
 class AppColors {
   static const Color primaryColor = Color(0xFF3F51B5); // blue
   static const Color secondaryColor = Color(0xFFFFFFFF); // white
